e828b6c7aa9afb430681aaa6779c8f50c0adfa42
simvelland <125514072+simvelland@users.noreply.github.com>
2023-11-06 15:53:46 -0500

    Create Chapter 5- SortedLinkedList.java

---
+++ Chapter 5- SortedLinkedList.java
@@ -0,0 +1,66 @@
+public class SortedLinkedList {
+
+    private Link first;
+
+    public SortedLinkedList() {
+
+        first = null;
+    }
+
+    public void insert(int key) {
+
+        Link newLink = new Link(key);
+        Link previous = null;
+        Link current = first;
+
+        while (current != null && key > current.getData()) {
+
+            previous = current;
+            current = current.getNext();
+        }
+
+        if (previous == null) {
+            first = newLink;
+        } else {
+            previous.setNext(newLink);
+        }
+        newLink.setNext(current);
+    }
+
+    public Link remove() {
+
+        Link temp = first;
+        first = first.getNext();
+        return temp;
+    }
+
+    public void displayList() {
+
+        Link current = first;
+        
+        while (current != null) {
+
+            current.displayLink();
+            current = current.getNext();
+        }
+    }
+
+    public boolean isEmpty() {
+
+        return first==null;
+    }
+
+    public int sumData() {
+
+        Link current = first;
+        int sum = 0;
+        
+        while (current != null) {
+
+            sum += current.getData();
+            current = current.getNext();
+        }
+
+        return sum;
+    }
+}
