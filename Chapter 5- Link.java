7dde93203fbed5e4f58a95e7f3d2a21675c1ff18
simvelland <125514072+simvelland@users.noreply.github.com>
2023-11-06 15:51:38 -0500

    Create Chapter 5- Link.java

---
+++ Chapter 5- Link.java
@@ -0,0 +1,30 @@
+public class Link {
+    
+    private int data;
+    private Link next;
+
+    public Link(int d) {
+
+        data = d;
+    }
+
+    public void displayLink() {
+
+        System.out.print(data + " ");
+    }
+
+    public Link getNext() {
+
+        return next;
+    }
+
+    public void setNext(Link n) {
+
+        next = n;
+    }
+
+    public int getData() {
+
+        return data;
+    }
+}
